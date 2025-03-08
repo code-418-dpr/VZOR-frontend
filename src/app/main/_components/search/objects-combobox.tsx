867a5610b009d6661550aa3f9db83ec4197a2619
V1ruses <vasya.ivanov2004.ivanov@mail.ru>
2025-03-09 02:16:33 +0300

    feat: multi combobox is drawer on mobile view

--- src/app/main/_components/search/objects-combobox.tsx
+++ src/app/main/_components/search/objects-combobox.tsx
@@ -6,7 +6,9 @@ import { JSX, useMemo, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { cn } from "@/lib/utils";
 
 export function ObjectsCombobox({
@@ -23,6 +25,48 @@ export function ObjectsCombobox({
     className?: string;
 }): JSX.Element {
     const [open, setOpen] = useState(false);
+    const isDesktop = useMediaQuery("(min-width: 768px)");
+
+    if (isDesktop) {
+        return (
+            <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                    <Button variant="outline" role="combobox" aria-expanded={open} className="flex gap-2 pr-2">
+                        <span>{title}</span>
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className={cn("w-full p-0", className)}>
+                    <ObjectList values={values} setValues={setValues} hideSearch={hideSearch} />
+                </PopoverContent>
+            </Popover>
+        );
+    }
+
+    return (
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>
+                <Button variant="outline" role="combobox" aria-expanded={open} className="flex gap-2 pr-2">
+                    <span>{title}</span>
+                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+            </DrawerTrigger>
+            <DrawerContent className={cn("w-full p-0", className)}>
+                <ObjectList values={values} setValues={setValues} hideSearch={hideSearch} />
+            </DrawerContent>
+        </Drawer>
+    );
+}
+
+function ObjectList({
+    values,
+    setValues,
+    hideSearch,
+}: {
+    values: Map<string, boolean>;
+    setValues: (values: Map<string, boolean>) => void;
+    hideSearch?: boolean;
+}) {
     const [search, setSearch] = useState("");
 
     const filteredItems = useMemo(() => {
@@ -34,38 +78,26 @@ export function ObjectsCombobox({
     }, [values, search]);
 
     return (
-        <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-                <Button variant="outline" role="combobox" aria-expanded={open} className="flex gap-2 pr-2">
-                    <span>{title}</span>
-                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className={cn("w-full p-0", className)}>
-                <Command shouldFilter={false}>
-                    {!hideSearch && <CommandInput placeholder="Поиск..." value={search} onValueChange={setSearch} />}
-                    <CommandList>
-                        <CommandEmpty>Ничего не найдено.</CommandEmpty>
-                        <CommandGroup>
-                            {filteredItems.slice(0, 500).map((key) => (
-                                <CommandItem
-                                    key={key}
-                                    value={key}
-                                    onSelect={(currentValue) => {
-                                        setValues(new Map(values).set(currentValue, !values.get(currentValue)));
-                                    }}
-                                    className="pr-4"
-                                >
-                                    <Check
-                                        className={cn("mr-1 h-4 w-4", values.get(key) ? "opacity-100" : "opacity-0")}
-                                    />
-                                    {key}
-                                </CommandItem>
-                            ))}
-                        </CommandGroup>
-                    </CommandList>
-                </Command>
-            </PopoverContent>
-        </Popover>
+        <Command shouldFilter={false}>
+            {!hideSearch && <CommandInput placeholder="Поиск..." value={search} onValueChange={setSearch} />}
+            <CommandList>
+                <CommandEmpty>Ничего не найдено.</CommandEmpty>
+                <CommandGroup>
+                    {filteredItems.slice(0, 500).map((key) => (
+                        <CommandItem
+                            key={key}
+                            value={key}
+                            onSelect={(currentValue) => {
+                                setValues(new Map(values).set(currentValue, !values.get(currentValue)));
+                            }}
+                            className="pr-4"
+                        >
+                            <Check className={cn("mr-1 h-4 w-4", values.get(key) ? "opacity-100" : "opacity-0")} />
+                            {key}
+                        </CommandItem>
+                    ))}
+                </CommandGroup>
+            </CommandList>
+        </Command>
     );
 }
