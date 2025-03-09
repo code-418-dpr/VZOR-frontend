da595b5608b1746a6ad4ee6e223634bbdfcf2d35
Scorpi-ON <s.scorpi-on@ya.ru>
2025-03-09 14:30:40 +0300

    fix(landing): improved footer responsibility

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,16 +4,16 @@ import siteMetadata from "@/conf/site-metadata";
 
 export default function Footer() {
     return (
-        <footer className="flex items-center border-t py-6 text-sm text-muted-foreground text-center">
-            <Link className="flex-1" href={siteMetadata.authors.url}>
+        <footer className="flex flex-col space-y-4 md:flex-row items-center border-t py-6 text-muted-foreground text-xs md:text-sm text-center">
+            <Link className="flex-1 hover:text-foreground" href={siteMetadata.authors.url} target="_blank">
                 Разработано командой <span className="gradient-text font-bold">{siteMetadata.authors.name}</span>
             </Link>
-            <Link className="flex-1" href="https://t.me/fspdnr/614">
+            <Link className="flex-1 hover:text-foreground" href="https://t.me/fspdnr/614" target="_blank">
                 Межрегиональный IT хакатон в рамках федерального проекта «Цифровая Россия»
                 <br />
                 Донецк, 14 — 15 марта 2025
             </Link>
-            <Link className="flex-1" href={siteMetadata.repo_url}>
+            <Link className="flex-1 hover:text-foreground" href={siteMetadata.repo_url} target="_blank">
                 Исходный код на GitHub
             </Link>
         </footer>
