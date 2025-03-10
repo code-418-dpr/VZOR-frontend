a42d60e6316dc059bb87319b4a1ae85b618829d5
Scorpi-ON <s.scorpi-on@ya.ru>
2025-03-11 02:40:13 +0300

    fix: some ui fixes

--- src/components/ui/date-range-picker.tsx
+++ src/components/ui/date-range-picker.tsx
@@ -41,7 +41,7 @@ export function DateRangePicker({ id, className }: React.HTMLAttributes<HTMLDivE
                 </PopoverTrigger>
                 <PopoverContent className="w-auto p-0" align="start">
                     <Calendar
-                        autoFocus
+                        initialFocus
                         mode="range"
                         defaultMonth={date?.from}
                         selected={date}
